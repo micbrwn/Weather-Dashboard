3f221ff328950565be4fa4e849479a4a74e5035d
Michael <Michael@Ms-MBA.local>
2022-06-07 17:53:01 -0400

    Fixed js and localstorage recall and created readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Weather-Dashboard
+
+This is a fully functioning weather-dashboard. When a city is searched, the dashboard will display data for for both current weather and 5-day forecast. Both will secitons will provide data for temp, humidity windspeed, a long with the date the weather is for. The current weather will also display uv index, which is alos color coded. 
+After a search is made, the city name will be made available in a "clickable" history list. When the selected search city is clicked on from the history list, the weather data for the selected city will be displayed again.
+
+
+## Criteria
+
+
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+
+## Screenshots
+
+
+![WeatherDash](./assets/Images/Screen%20Shot%202022-06-07%20at%205.33.48%20PM.png)
+
+![WeatherDash](./assets/Images/Screen%20Shot%202022-06-07%20at%205.34.06%20PM.png)
+
+
+## Webpage
+
+https://micbrwn.github.io/Weather-Dashboard/
--- assets/script.js
+++ assets/script.js
@@ -3,92 +3,74 @@ $(document).ready(function () {
 
   //====================================================================== Global variables ===================================================================
 
-  // Global variables to be used
   var searchBtn = document.getElementById('searchBtn');
+  var deleteBtn = document.getElementById('deleteBtn');
   var formEl = document.getElementById('form1');
   var cityList = document.getElementById('city-list');
   var weatherDiv = document.getElementById('weather-container');
   var currentHead = document.getElementById('current-heading');
   var weatherHead = document.getElementById('weather-head');
   var forecast = document.getElementById('forecast');
-  // var weather = document.getElementById('weather');
-  // var foreDiv = document.getElementById('fore-container');
-  // var weatherCard = $("<div>").addClass("card");
   var weatherCard = document.createElement('div');
-      weatherCard.classList.add('card-body');
+  weatherCard.classList.add('card-body');
   var weatherCard1 = document.createElement('div');
-      weatherCard1.classList.add('card-body');
+  weatherCard1.classList.add('card-body1');
   var title = document.createElement('h3');
-      title.classList.add('weather-title');
-      
-  // var title = $("<h3>").addClass('weather-title');
+  title.classList.add('weather-title');
 
 
   // =============================================================== API call for geoloaction (lat/lon) ========================================================
   // Get lat and lon for city entered in input. Lat and lon is needed to get weather data
 
-  function getGeoLocation() {
+  function getGeoLocation(searchInput) {
     var apiKey = '312bdb80adf7dd8b05eda8e9064d9b1a';
-    var searchInput = formEl.value;
     var url = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchInput + '&limit=&appid=' + apiKey;
-    console.log(searchInput); // null
 
     fetch(url)
       .then(function (response) {
-        // console.log('geolocation', response); //data
-        // var lat = re
         return response.json();
       })
 
       .then(function (data) {
-        console.log('geolocation', data); // array of data including lat & lon
         var lat = data[0].lat;
         var lon = data[0].lon;
-        console.log(lat, lon);
         var name = data[0].name;
         currentHead.style.display = 'none';
-        title.textContent = (data[0].name);
-        // weatherHead.appendChild(title);
-        //  return data;
+        title.textContent = (name);
 
-        console.log(name);
-        // weatherCard.innerHTML = "";
-        // forecast.innerHTML = "";
-        console.log('weather card:', weatherCard);
-        console.log('forecast', forecast);
+        //================================================================= Display search city =======================================================
+
+        weatherCard.textContent = "";
+        weatherCard1.textContent = "";
+        forecast.textContent = "";
         forecast.append(weatherCard);
         getCurrenWeather(lat, lon);
-        // historyList();
       });
   }
   //============================================================== API call for weather data =============================================================
 
-  function getCurrenWeather(lat, lon, name) {
+  function getCurrenWeather(lat, lon) {
     var apiKey = '312bdb80adf7dd8b05eda8e9064d9b1a';
     var queryUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=imperial';
 
     fetch(queryUrl)
       .then(function (response) {
-        // console.log('current weather', response);
         return response.json();
       })
 
       .then(function (data) {
-        console.log('current weather', data);
 
-
-  //===================================================================== Get Current Weather ================================================================== 
+        //===================================================================== Get Current Weather ================================================================== 
 
         var cardContent1 = document.createElement('div');
-          cardContent1.classList.add('card-text');  
-          
+        cardContent1.classList.add('card-text1');
+
         var dt1 = data.daily[0].dt;
         var date1 = document.createElement('p');
         date1.innerHTML = new Date(dt1 * 1000).toDateString();
-        console.log("date/time", date1);
 
         var icon1 = document.createElement('img');
-        icon1 = ('src', "http://openweathermap.org/img/wn/" + data.daily[0].weather[0].icon + ".png");
+        icon1.setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[0].weather[0].icon + ".png");   // Doesn't work 
 
         var temp1 = document.createElement('p');
         temp1.innerHTML = "Temp (F): " + Math.floor(data.daily[0].temp.day)
@@ -97,45 +79,43 @@ $(document).ready(function () {
         humidity1.innerHTML = "Humidity: " + Math.floor(data.daily[0].humidity);
 
         var windSpeed1 = document.createElement('p');
-        windSpeed1.innerHTML = "Wind Speed (MPH): "  + Math.floor(data.daily[0].wind_speed);
+        windSpeed1.innerHTML = "Wind Speed (MPH): " + Math.floor(data.daily[0].wind_speed);
 
         //============================================================================ Get UV index =========================================================================
 
-          var uvIndex = document.createElement('p');
-          uvIndex.innerHTML = "UV Index: " + data.current.uvi;
+        var uvIndex = document.createElement('p');
+        uvIndex.classList.add('uvIndex');
+        uvIndex.innerHTML = "UV Index: " + data.current.uvi;
+        var currentUvi = data.current.uvi;
 
-          if (uvIndex <= 2.99) {                  
-            uvIndex = $("#uvIndex").css({"background-color": "olivedrab", "display": "block", "border-radius": "12px", "padding": "1.5%", "max-width": "20%"});
-        } else if (uvIndex >= 3 & uvIndex <= 5.99) {
-            uvIndex = $("#uvIndex").css({"background-color": "gold", "display": "block", "border-radius": "12px", "padding": "1.5%", "max-width": "20%"});
-        } else if (uvIndex >= 6 & uvIndex <= 7.99) {
-            uvIndex = $("#uvIndex").css({"background-color": "darkorange", "display": "block", "border-radius": "12px", "padding": "1.5%", "max-width": "20%"});
-        } else if (uvIndex >= 8) {
-            uvIndex = $("#uvIndex").css({"background-color": "firebrick", "display": "block", "border-radius": "12px", "padding": "1.5%", "max-width": "20%"});
+        if (currentUvi <= 2.99) {
+          uvIndex.style.cssText = 'background-color: green; display: block; border-radius: 10px; padding: 5px;';
+        } else if (currentUvi >= 3 && currentUvi <= 5.99) {
+          uvIndex.style.cssText = 'background-color: gold; display: block; border-radius: 10px; padding: 5px;';
+        } else if (currentUvi >= 6 && currentUvi <= 7.99) {
+          uvIndex.style.cssText = 'background-color: darkorange; olivedrab; display: block; border-radius: 10px; padding: 5px;';
+        } else {
+          uvIndex.style.cssText = 'background-color: firebrick; display: block; border-radius: 10px; padding: 5px;';
         };
 
         //======================================================================= Display current weather and UV index =============================================================
 
-        console.log(cardContent1);
-        cardContent1.append(date1, temp1, humidity1, windSpeed1, uvIndex);
+        cardContent1.append(date1, icon1, temp1, humidity1, windSpeed1, uvIndex);    
         weatherCard1.append(cardContent1);
-        console.log(weatherCard1);
         weatherDiv.append(weatherCard1);
 
-      //========================================================================== Get 5-day Forecast ====================================================================  
+        //========================================================================== Get 5-day Forecast ====================================================================  
 
         for (var i = 1; i < 6; i++) {
           var cardContent = document.createElement('div');
-            cardContent.classList.add('card-text');
+          cardContent.classList.add('card-text');
 
           var dt = data.daily[i].dt;
           var date = document.createElement('p');
           date.innerHTML = new Date(dt * 1000).toDateString();
-          console.log("date/time", date);
 
-          // var icon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png");
           var icon = document.createElement('img');
-          icon = ('src', "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png");
+          icon.setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png");
 
           var temp = document.createElement('p');
           temp.innerHTML = "Temp (F): " + Math.floor(data.daily[i].temp.day)
@@ -144,8 +124,7 @@ $(document).ready(function () {
           humidity.innerHTML = "Humidity: " + Math.floor(data.daily[i].humidity);
 
           var windSpeed = document.createElement('p');
-          windSpeed.innerHTML = "Wind Speed (MPH): "  + Math.floor(data.daily[i].wind_speed);
-          // cardContent.attr('style', "border-radius:20px;");
+          windSpeed.innerHTML = "Wind Speed (MPH): " + Math.floor(data.daily[i].wind_speed);
 
           var today = document.createElement('h3');
           var today = data.daily[0].dt;
@@ -153,48 +132,52 @@ $(document).ready(function () {
 
           //======================================================================== Display 5-day Forecast ===================================================================
 
-          console.log(cardContent);
-          cardContent.append(date, temp, humidity, windSpeed);
+          cardContent.append(date, icon, temp, humidity, windSpeed);
           weatherCard.append(cardContent);
-          console.log(weatherCard);
           forecast.append(weatherCard);
-          console.log(forecast);
           weatherHead.appendChild(title);
-     
         }
-        
       });
   }
 
   //=============================================================================== Create a list of search histoy ================================================================
 
   function historyList() {
-    // cityList.innerHTML = "";
+    cityList.textContent = "";
     var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-    
+
     searchHistory.forEach(function (search) {
-      // var button = $("<button>").addClass("btn").text(search);
       var button = document.createElement('button');
       button.classList.add('btn')
       button.textContent = search;
+      button.style.cssText = 'background-color: lightblue; margin: 5px; border-radius: 5px;'
       button.addEventListener("click", function () {
+        console.log(search);
         getGeoLocation(search);
       });
       cityList.append(button);
     })
   }
 
-
   //============================================================================== Start event with click of search button ==============================================================
 
   searchBtn.addEventListener('click', function () {
-    var searchInput = formEl.value;
+    var searchInput = formEl.value.trim();
     getGeoLocation(searchInput);
-    var searchHistory = JSON.parse(localStorage.getItem('search-history')) || [];
-    searchHistory.push(searchInput); // push(searchHistory)?
+    var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+
+    deleteBtn.addEventListener('click', function () {
+      localStorage.clear();
+      cityList.textContent = "";
+    })
+
+
+    searchHistory.push(searchInput);
     localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
     historyList();
   });
 
-})
+
+
+});
 
